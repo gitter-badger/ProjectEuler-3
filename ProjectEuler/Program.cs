f2ea17969cb6ad8f4b3e50f891a7430fa102523e
George Kampolis <gkampolis@outlook.com>
2017-08-23 11:06:19 +0300

    Tryout at VS Code, scafolding, possibly first problem solution, ready for further testing.

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,28 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunSet1();
         }
-    }
+
+        static void RunSet1();
+        {
+            Console.WriteLine(string "Find the sum of all the multiples of x or y below z. \nAll must be integers.");
+            int x = ReadNumber(string "Please enter x:");
+            int y = ReadNumber(string "Please enter y:");
+            int z = ReadNumber(string "Please enter z:");
+            Console.WriteLine("Answer: {0}", Set1.Problem1(x,y,z));
+        }
+    
+        static double ReadNumber(string args)
+        {
+            Console.WriteLine(args);
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Invalid number format");
+            }
+            return number;
+
+        }
+}
 }
---
+++ ProjectEuler/Set1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public static class Set1
+    {
+        public int Problem1(int a, int b, int c)
+        {
+            int sum = 0
+            for (int i = 1; i < c; i++)
+            {
+                if (i % 3 = 0 || i % 5 = 0)
+                {
+                    sum += i
+                }
+
+            }
+        }
+    }
+}
