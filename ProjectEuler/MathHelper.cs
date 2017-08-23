190beb83651dfaf6ddbce91f92db7d16e6451aa5
Alexandros Kritikos <akritikos@outlook.com>
2017-08-23 14:07:30 +0300

    Added a class with helper methods for common mathematical problems

---
+++ ProjectEuler/MathHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler
+{
+	public static class MathHelper
+	{
+		public static int SumDivisibleBy(int divider, int max)
+		{
+			return divider * (max / divider) * ((max / divider) + 1) / 2;
+		}
+	}
+}
