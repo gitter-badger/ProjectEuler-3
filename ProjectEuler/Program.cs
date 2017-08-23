e38da451465412a87a7702376f6f9f68f8aeafa7
Alexandros Kritikos <akritikos@outlook.com>
2017-08-23 13:59:45 +0300

    Removed redundant method (merged with main) and changed to simple value types (var)

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,19 +6,14 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            RunSet1();
-        }
-
-        static void RunSet1()
-        {
-            Console.WriteLine("Find the sum of all the multiples of x or y below z. \nAll must be integers.");
-            int x = (int) ReadNumber("Please enter x:");
-            int y = (int) ReadNumber("Please enter y:");
-            int z = (int) ReadNumber("Please enter z:");
-            Console.WriteLine("Answer: {0}", Set1.Problem1(x,y,z));
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
+			Console.WriteLine("Find the sum of all the multiples of x or y below z. \nAll must be integers.");
+	        var x = (int)ReadNumber("Please enter x:");
+	        var y = (int)ReadNumber("Please enter y:");
+	        var z = (int)ReadNumber("Please enter z:");
+	        Console.WriteLine("Answer: {0}", Set1.Problem1(x, y, z));
+	        Console.WriteLine("Press any key to exit...");
+	        Console.ReadKey();
+		}
 
 		/// <summary>
 		/// Wrapper for sanitized input from console
